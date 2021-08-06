3be8c845fdb57466ce7ffbe3b64e60e075040f32
Taichi <tu.0514.0315@icloud.com>
2021-08-06 12:21:24 +0900

    extract ReminderListCell configuration

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -36,11 +36,9 @@ extension ReminderListViewController {
         }
         let reminder = Reminder.testData[indexPath.row]
         let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)
-        cell.doneButtonAction = {
+        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
             Reminder.testData[indexPath.row].isComplete.toggle()
+            tableView.reloadRows(at: [indexPath], with: .none)
         }
         return cell
     }
